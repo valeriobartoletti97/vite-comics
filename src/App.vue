def4fba56700ffe3d54911e6067937f8e1e192b4
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-09 17:37:45 +0100

    initialize and reset files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  ciao
+</template>
+
+<script>
+  export default {
+    name:'App',
+    data(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
