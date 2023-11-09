3a754745f704526b2c320313891134b01a9b2816
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-09 19:44:05 +0100

    Header finished

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -2,6 +2,13 @@
   <div>
       <header class="container d-flex justify-content-between">
          <img src="/images/dc-logo.png" alt="logo">
+         <ul class="nav d-flex justify-content-between align-items-center flex-nowrap ">
+            <li class="nav-item px-3 text-uppercase" v-for="(element,index) in navSections" :key="index">
+                <a :class="{'active-section' : element.active=== true}" :href="href">
+                    {{ element.text }}
+                </a>
+            </li>
+         </ul>
       </header>
   </div>
 </template>
@@ -9,6 +16,62 @@
 <script>
     export default {
         name: 'HeaderApp',
+        data(){
+            return{
+                navSections: [
+                    {
+                        text:'Characters',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Comics',
+                        href:'#',
+                        active: true
+                    },
+                    {
+                        text:'Movies',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Tv',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Games',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Collectibles',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Videos',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Fans',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'News',
+                        href:'#',
+                        active: false
+                    },
+                    {
+                        text:'Shop',
+                        href:'#',
+                        active: false
+                    },
+                ]
+            }
+        }
     }
 </script>
 
@@ -17,6 +80,16 @@
         padding:20px 0;
         img{
             width:80px;
+        }
+        li{
+            font-weight: 800;
+        }
+        a{
+            text-decoration: none;
+            color:black
+        }
+        .active-section{
+            color: #0282f9;
         }
      }
 </style>
