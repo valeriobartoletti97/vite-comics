9e3935e73c010dbff8dbac827b0ad41925483549
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-11-10 15:44:24 +0100

    working on footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,6 +1,13 @@
 <template>
 <footer>
-  footer
+  <div class="container d-flex justify-content-between flex py-4">
+    <button class="text-uppercase py-3">
+      <a class="p-3" href="#">Sign-up now!</a>
+    </button>
+    <div class="d-flex justify-content-between">
+      <h4 class="text-uppercase">Follow us</h4>
+    </div>
+  </div>
 </footer>
 </template>
 
@@ -11,5 +18,12 @@
 </script>
 
 <style lang="scss">
-
+@use '../assets/partials/variables' as *;
+    footer{
+      background-color: $secondary-color;
+      button{
+        background-color: $secondary-color;
+        border: 2px solid $primary-color;
+      }
+    }
 </style>
