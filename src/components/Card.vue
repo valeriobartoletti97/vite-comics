8d7f27293f53d2b6bc2510902a0ba2ed66ab2873
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-11-11 20:08:23 +0100

    created card component and printed in contentapp

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="my-card">
+        <div class="img-box">
+            <img :src="img" :alt="title">
+        </div>
+        <h5 class="text-uppercase pt-3">
+            {{title}}
+        </h5>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    img: String,
+    title: String,
+    alt: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.my-card{
+  width: 200px;
+  .img-box {
+    width: 200px;
+    height: 200px;
+    overflow: hidden;
+    cursor: pointer;
+    img{
+        object-fit: contain;
+        width: 100%
+    }
+  }
+  h5{
+        color: white;
+        font-size: 1.4em;
+        font-weight: 700
+    }
+}
+</style>
--- src/components/ContentApp.vue
+++ src/components/ContentApp.vue
@@ -1,25 +1,48 @@
 <template>
-    <div class="bg-black">
-      <section class="container">
-        <h1 class="text-white py-5"> {{ title }} </h1>
-      </section>
+    <div class="content-app">
+      <div class="jumbo-main">
+
+      </div>
+      <div class="bg-black">
+        <div class="container">
+          <div class="row flex-wrap justify-content-between py-5">
+            <div class="col-2 d-flex flex-wrap px-2" v-for="(card,index) in comicsArray" :key="index">
+              <Card :img="card.thumb" :title="card.series" :alt="card.type"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+import {comics} from '../data/dc-comics.js';
+import Card from './Card.vue';
     export default {
         name: 'ContentApp',
+        components:{
+          Card
+        },
         data(){
           return{
-            title: '--> Content goes here <--'
+            comicsArray: comics
           }
         }
     }
 </script>
 
 <style lang="scss" >
-    .bg-black{
-      background-color:black
+    .content-app{
+      .jumbo-main{
+        height: 450px;
+        overflow: hidden;
+        background-image: url('/images/jumbotron.jpg');
+        background-repeat: no-repeat;
+        background-position-x: center;
+      }
+      .bg-black{
+        background-color: black;
+      }
     }
     h1{
       font-size: 2em;
